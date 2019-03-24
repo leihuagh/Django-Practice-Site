44a5a596ef7c574545017e45b41a961080060f4a
Lei Hua <lhua@example.com>
2019-03-24 11:50:59 -0400

    routes

--- core/views.py
+++ core/views.py
@@ -2,4 +2,24 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    context = {
+        "title": "Welcome",
+        "header": "Home Index"
+    }
+    return render(request, 'index.html', context)
+
+
+def about(request):
+    context = {
+        "title": "About Us",
+        "header": "About Us"
+    }
+    return render(request, 'index.html', context)
+
+
+def contact(request):
+    context = {
+        "title": "Contact Us",
+        "header": "Contact Us"
+    }
+    return render(request, 'index.html', context)

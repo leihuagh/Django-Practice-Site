175428ef1bc754979546680a364a713b2bb7ce24
Lei Hua <lhua@example.com>
2019-05-26 12:04:47 -0400

    logout works

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from .views import index, about, contact
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('about', about, name='about'),
     path('contact', contact, name='contact'),
+    path('logout', LogoutView.as_view(
+        template_name='logout.html'), name='logout'),
 ]

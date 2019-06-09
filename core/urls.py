2ac0c3aa6ec3315af931d7be1c517be183ed3d13
Lei Hua <lhua@example.com>
2019-06-09 11:47:58 -0400

    add login page

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, LoginView
 from .views import index, about, contact
 
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('contact', contact, name='contact'),
     path('logout', LogoutView.as_view(
         template_name='logout.html'), name='logout'),
+    path('login', LoginView.as_view(template_name='login.html'), name='login'),
 ]
